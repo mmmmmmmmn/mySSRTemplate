332f8cf77df88eb016592dcc3190c5c8d89e18ba
mmmmmmmmn <mygithubemailllll@gmail.com>
2021-01-03 16:45:23 +0900

    change dev process

--- src/server/index.tsx
+++ src/server/index.tsx
@@ -6,7 +6,9 @@ import App from '../client/App'
 import express from 'express'
 
 const app = express()
+
 app.use(express.static('dist'))
+app.get('/*.js(.map)?', (req, res) => res.redirect(`http://${host}:8080/${req.url}`))
 
 app.get('*', (req, res) => {
     const sheet = new ServerStyleSheet()
